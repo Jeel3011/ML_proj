7b6ba2e6348c8a1d3402ab70094f2a16f1c09902
Jeel3011 <jeel15thummar@gmail.com>
2025-12-12 23:53:28 +0530

    commit

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -35,16 +35,18 @@ class ModelTrainer:
             )
 
             models = {
+                
                 "Linear Regression": LinearRegression(),
                 "Ridge": Ridge(),
                 "Lasso": Lasso(),
                 "ElasticNet": ElasticNet(),
                 "K-Neighbors Regressor": KNeighborsRegressor(),
-                "Decision Tree": DecisionTreeRegressor(),,
+                "Decision Tree": DecisionTreeRegressor(),
                 "XGB Regressor": xgb.XGBRegressor(),
-                "CatBoost Regressor": CatBoostRegressor(verbose=False)
+                "CatBoost Regressor": CatBoostRegressor(verbose=False),
                 "Random Forest": RandomForestRegressor(),
-                "AdaBoost Regressor": AdaBoostRegressor()
+                "AdaBoost Regressor": AdaBoostRegressor(),
+
             }
 
 
@@ -54,9 +56,9 @@ class ModelTrainer:
                 "Lasso": {"alpha": [0.1,1.0,10.0]},
                 "ElasticNet": {"alpha": [0.1,1.0,10.0], "l1_ratio": [0.1,0.5,0.9]},
                 "K-Neighbors Regressor": {"n_neighbors": [3,5,7]},
-                "Decision Tree": {"max_depth": [3,5,7]},,
+                "Decision Tree": {"max_depth": [3,5,7]},
                 "XGB Regressor": {"n_estimators": [50,100,200], "learning_rate": [0.01,0.1,0.2]},
-                "CatBoost Regressor": {"iterations": [50,100,200], "learning_rate": [0.01,0.1,0.2]}
+                "CatBoost Regressor": {"iterations": [50,100,200], "learning_rate": [0.01,0.1,0.2]},
                 "Random Forest": {"n_estimators": [50,100,200], "max_depth": [3,5,7]},
                 "AdaBoost Regressor": {"n_estimators": [50,100,200]}
             }
